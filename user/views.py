042ca367e44fcfaf8529e0d4bc4d01e74286e326
huapenghui <xiaomin@169.com>
2020-06-25 18:23:11 +0800

    路由管理

---
+++ user/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
+# Create your views here.
+
+def test(request):
+
+
+    "fdgflml"
+    "fdsg"\
+    "134"
+    "123"
+    "545"
+    return HttpResponse(request,"test1.html")
